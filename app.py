41bc113118a6e8e1d9bbc97d6cc821bbf18686e9
yourlow <m.holloway@uq.net.au>
2023-08-08 13:11:44 +1000

    added env and some logging, and refactoring

--- app.py
+++ app.py
@@ -4,50 +4,115 @@ import time
 from escpos.printer import Usb
 from escpos.constants import PAPER_PART_CUT
 import os
+from dotenv import load_dotenv
+from datetime import datetime
+import logging
+load_dotenv()
 
+def check_env():
+    if os.getenv("DB_HOST") is None:
+        logging.error("DB_HOST not set")
+        exit(1)
+    if os.getenv("DB_PORT") is None:
+        logging.error("DB_PORT not set")
+        exit(1)
+    if os.getenv("DB_USER") is None:
+        logging.error("DB_USER not set")
+        exit(1)
+    if os.getenv("DB_PASSWORD") is None:
+        logging.error("DB_PASSWORD not set")
+        exit(1)
+    if os.getenv("DB_DATABASE") is None:
+        logging.error("DB_DATABASE not set")
+        exit(1)
+
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', 
+                    level=logging.INFO, 
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("Starting Printer Service")
 # Determine the directory of the current script
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the relative path
 logoImage = os.path.join(script_directory, './logoReduced.jpg')
 
-# Connect to the MySQL database
-mysql_connection = mysql.connector.connect(
-  host="kingsliff-sands-trucking-database-do-user-8801771-0.b.db.ondigitalocean.com",
-  port=25060,
-  user="doadmin",
-  password="bkoyee0cmuewh3if",
-  database="loadmaster_prod"
-)
+mysql_connection = None
+
+logging.info("Connecting to Database")
+
+check_env()
+
+
+
+try:
+    mysql_connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_DATABASE")
+    )
+
+except Exception as e: 
+    logging.error("Failed to connect to Database")
+    exit(1)
+
+if mysql_connection.is_connected():
+    logging.info("Connected to Database")
+
+else:
+    logging.error("Failed to connect to Database")
+    exit()
+
+
+
 
 def job():
-
+    # logging.info("Checking Database")
     mysql_connection.start_transaction(isolation_level='READ COMMITTED')
     # Create a new cursor
     cursor = mysql_connection.cursor(dictionary=True)
 
     # Query the MySQL database
     cursor.execute(f"""SELECT * FROM `Detailed Records` WHERE printed = 0""")
-
+    rows = cursor.fetchall()
     jobnumbers_to_update = []
-    
-    # Iterate over each record
-    for record in cursor:
-        jobnumber = record['JobNumber']
-        print_driver_docket(record)
-        print_customer_docket(record)
+    if(len(rows) != 0):
+        logging.info(f"Found {len(rows)} record{'s' if len(rows) > 1 else ''} to print")
+        # Iterate over each record
+        for record in rows:
+            logging.info(f"Printing JobNumber: {record['JobNumber']}")
 
-        jobnumbers_to_update.append(jobnumber)
+            #@Todo check each record for errors
+            try:
 
-    # Update all records outside the loop
-    for jobnumber in jobnumbers_to_update:
-        cursor.execute('UPDATE record SET printed = 1 WHERE JobNumber = %s', (jobnumber,))
+                logging.info(f"Printing Driver Docket for JobNumber: {record['JobNumber']}")
+                print_driver_docket(record)
+                logging.info(f"Printing Customer Docket for JobNumber: {record['JobNumber']}")
+                print_customer_docket(record)
+                jobnumber = record['JobNumber']
+                jobnumbers_to_update.append(jobnumber)
 
+                logging.info("Finished Printing, Updating Database")
+                # Update all records outside the loop
+                for jobnumber in jobnumbers_to_update:
+                    cursor.execute('UPDATE record SET printed = 1 WHERE JobNumber = %s', (jobnumber,))
+
+            except:
+                logging.error(f"Failed to print JobNumber: {record['JobNumber']}")
+                continue
+
+       
     mysql_connection.commit()
     cursor.close()
 
 
 def print_docket_details(printer: Usb, record) -> None:
+    """"Prints the docket details
+        Takes a printer object and a record object
+    """
     printer.text(f"Job Number: {record['JobNumber']}\n")
     printer.text(f"Load Time: {record['EndTimeDate']}\n")
     printer.text(f"Unit Weight: {record['UnitWeight']} t\n")
@@ -60,7 +125,7 @@ def print_docket_details(printer: Usb, record) -> None:
 
 
 def print_spacing(printer: Usb, spacing: int) -> None:
-    for i in range(10):
+    for i in range(spacing):
         printer._raw(bytes([10]))
 
 def cut_docket(printer: Usb) -> None:
@@ -68,8 +133,16 @@ def cut_docket(printer: Usb) -> None:
 
 
 def print_driver_docket(record):
+    """
+    Prints the driver docket
+        Throws an exception if the printer has an error
+        record: a record object
+    """
     try:
         printer = Usb(idVendor=0x03F0, idProduct=0x0E69, in_ep=0x82, out_ep=0x02)
+
+        #do printer checks
+
         printer.set(align='center', text_type='B', width=2, height=2)
 
         printer.text(f"Driver Docket\n")
@@ -87,8 +160,8 @@ def print_driver_docket(record):
         cut_docket(printer)
 
     except Exception as e:
-        print(e, flush=True)
-
+        logging.error(f"Failed to print driver docket, {e}")
+        raise e
 
 def print_customer_docket(record):
     try:
@@ -112,40 +185,10 @@ def print_customer_docket(record):
     except Exception as e:
         print(e, flush=True)
 
-# Define a function to print the docket
-def print_docket(record):
-    try:
-        printer = Usb(idVendor=0x03F0, idProduct=0x0E69, in_ep=0x82, out_ep=0x02)
 
-        printer.text(f"Job Number: {record['JobNumber']}\n")
-        printer.text(f"Load Time: {record['EndTimeDate']}\n")
-        printer.text(f"Unit Weight: {record['UnitWeight']} t\n")
-        printer.text(f"Product: {record['product_name']}\n")
-        printer.text(f"Customer: {record['customer_name']}\n")
-        printer.text(f"Destination: {record['destination_name']}\n")
-        printer.text(f"Haulier: {record['haulier_name']}\n")
-        printer.text(f"Truck: {record['truck_name']}\n")
-        printer.text(f"CA:{record['note']}\n")
-
-        printer.text("--------------------------------\n")
-
-        printer._raw(bytes([10]))
-        printer._raw(bytes([10]))
-        printer._raw(bytes([10]))
-
-        printer.image("./logoReduced.jpg")
-        printer._raw(bytes([10]))
-        printer._raw(bytes([10]))
-        printer._raw(bytes([10]))
-        printer._raw(bytes([10]))
-        printer._raw(bytes([10]))
-        printer._raw(bytes([29, 86, 1, 19]))
-
-    except Exception as e:
-        print(e, flush=True)
-
+logging.info("Starting Scheduler")
 # Schedule the job every 10 seconds
-schedule.every(1).seconds.do(job)
+schedule.every(10).seconds.do(job)
 
 # Keep the script running
 while True:
