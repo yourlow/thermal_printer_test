807584a092fb56c9206781814f10e231b43950f2
yourlow <m.holloway@uq.net.au>
2024-06-03 14:40:14 +1000

    updated time string

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def print_job(jobs):
 
 
 def convert_utc_to_sydney(utc_time_str):
-    utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ")
+    utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%S.%fZ")
     utc_time = utc_time.replace(tzinfo=pytz.UTC)
     sydney_tz = pytz.timezone('Australia/Sydney')
     sydney_time = utc_time.astimezone(sydney_tz)
